8ad2106919421cfe4da6395508315671c277426e
husnil maarif <husnilmaarif.hm@gmail.com>
2022-05-06 17:18:29 +0800

    data produk dari database belum bisa ditampilkan dibagian client

---
+++ routes/productsRoute.js
@@ -0,0 +1,14 @@
+// const express = require("express");
+// const router = express.Router();
+// const productModel = require("../models/productModel");
+
+// router.get("/getAllProducts", async (req, res) => {
+//   try {
+//     const products = await productModel.find({});
+//     res.send(products);
+//   } catch (error) {
+//     return res.status(400).json({ message: error });
+//   }
+// });
+
+// module.exports = router;
--- server.js
+++ server.js
@@ -3,23 +3,16 @@ const app = express();
 const port = process.env.PORT || 5000;
 const connectDB = require("./db");
 const ProductDB = require("./models/productModel");
+const productsRoute = require("./routes/productsRoute");
 
 connectDB();
 
 app.use(express.json());
 
+app.use("/api/products", productsRoute);
+
 app.get("/", (req, res) => {
   res.send("Halo Dunia!");
-});
-
-app.get("/getProduct", (req, res) => {
-  ProductDB.find({}, (err, docs) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.send(docs);
-    }
-  });
 });
 
 app.listen(port, () => {
