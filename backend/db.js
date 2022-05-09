8f4d7c8c097fdc195fdebbd6d168631e717692cd
husnil maarif <husnilmaarif.hm@gmail.com>
2022-05-09 22:13:16 +0800

    membuat halaman registrasi

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://User-123:user-123@final-project-eduwork.kp7cf.mongodb.net/produk-pos-eduwork", {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,27 @@ const port = process.env.PORT || 5000;
 const connectDB = require("./db");
 const ProductDB = require("./models/productModel");
 const productsRoute = require("./routes/productsRoute");
+const userRoute = require("./routes/userRoute");
 
 connectDB();
 
 app.use(express.json());
 
 app.use("/api/products", productsRoute);
+app.use("/api/users", userRoute);
 
 app.get("/", (req, res) => {
   res.send("Halo Dunia!");
+});
+
+app.get("/getAllProducts", (req, res) => {
+  ProductDB.find({}, (err, docs) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(docs);
+    }
+  });
 });
 
 app.listen(port, () => {
